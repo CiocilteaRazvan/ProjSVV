a1f48e4ea3c04944bbd4a04f06e348f5734dd315
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-10-30 17:55:27 +0300

    Added nested class for testing strToInt() method of StringConv class + added more one digit tests

--- src/test/java/lab1/StringConvTest.java
+++ src/test/java/lab1/StringConvTest.java
@@ -1,8 +1,6 @@
 package lab1;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -21,8 +19,14 @@ public class StringConvTest {
         assertTrue(false);
     }
 
-    @Test
-    void testStrToIntOneDigit() {
-        assertEquals(0, strConv.strToInt("0"));
+    @Nested
+    @DisplayName("Test strToInt() method from StringConv class")
+    class TestStrToInt {
+        @Test
+        void testOneDigit() {
+            assertEquals(0, strConv.strToInt("0"));
+            assertEquals(8, strConv.strToInt("8"));
+            assertEquals(3, strConv.strToInt("3"));
+        }
     }
 }
