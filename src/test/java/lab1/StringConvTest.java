2b790826d23c53d1366ffc0ba43257529cb72bb9
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-10-30 17:50:13 +0300

    Created strConv object in test class and added test for strToInt with one digit

--- src/test/java/lab1/StringConvTest.java
+++ src/test/java/lab1/StringConvTest.java
@@ -1,16 +1,28 @@
 package lab1;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringConvTest {
+    static StringConv strConv;
+
+    @BeforeAll
+    static void init() {
+        strConv = new StringConv();
+    }
+
     @Disabled
     @Test
     void testDisabledAnnotation() {
         assertTrue(false);
     }
 
-
+    @Test
+    void testStrToIntOneDigit() {
+        assertEquals(0, strConv.strToInt("0"));
+    }
 }
