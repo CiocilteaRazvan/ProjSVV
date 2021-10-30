b4413bb2d1b5207ffe0bd672be32489ad3d18bc2
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-10-30 17:34:52 +0300

    Checked @Disabled functionallity

---
+++ src/test/java/lab1/StringConvTest.java
@@ -0,0 +1,16 @@
+package lab1;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringConvTest {
+    @Disabled
+    @Test
+    void testDisabledAnnotation() {
+        assertTrue(false);
+    }
+
+
+}
