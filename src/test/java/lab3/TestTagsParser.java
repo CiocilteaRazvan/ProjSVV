3473d8cbd13d1715ad87bf942d46e165fd143eac
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-10-31 11:28:50 +0200

    Changed test methods to allign tested method with test name

--- src/test/java/lab3/TestTagsParser.java
+++ src/test/java/lab3/TestTagsParser.java
@@ -4,17 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-
 public class TestTagsParser {
 	@Test
 	public void testCounter() {
-		TagsParser parser = new TagsParser(HelperClass.getUrlForPage("TestPage.html"));
-		assertEquals(6, parser.getTagsCount());
+		TagsParser parser = new TagsParser(HelperClass.getUrlForPage("TestPage.html")) ;
+		assertEquals("head", parser.getTagAt(2));
 	}
 	
 	@Test
 	public void testGetTagsCount(){
-		TagsParser parser = new TagsParser(HelperClass.getUrlForPage("TestPage.html")) ;
-		assertEquals("head", parser.getTagAt(2));
+		TagsParser parser = new TagsParser(HelperClass.getUrlForPage("TestPage.html"));
+		assertEquals(6, parser.getTagsCount());
 	}
 }
