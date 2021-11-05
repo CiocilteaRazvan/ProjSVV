d5440169581d570a632ca6620d2801dad7ec05de
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-11-05 15:07:38 +0200

    Created web client test

---
+++ src/test/java/webclient/WebClientTest.java
@@ -0,0 +1,14 @@
+package webclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WebClientTest {
+    private WebClient webClient;
+
+    @Test
+    void testWebClientReadWrite() {
+        assertTrue(true);
+    }
+}
