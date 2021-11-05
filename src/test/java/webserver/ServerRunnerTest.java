c6b5761a5c59e55a506700b45fd9b0a1cb1de285
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-11-05 15:30:08 +0200

    Added test for ServerRunner

---
+++ src/test/java/webserver/ServerRunnerTest.java
@@ -0,0 +1,14 @@
+package webserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServerRunnerTest {
+    ServerRunner serverRunner;
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
