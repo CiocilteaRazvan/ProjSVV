5bc14b8ccdb58631eff3e42389493a84a19c328f
CiocilteaRazvan <ciociltearazvan@gmail.com>
2021-11-06 20:18:23 +0200

    Moved things around ServerMockContainer

--- src/test/java/utils/ServerMockContainer.java
+++ src/test/java/utils/ServerMockContainer.java
@@ -6,22 +6,9 @@ import java.net.Socket;
 
 public class ServerMockContainer {
     private Socket mockSocket;
-    private BufferedReader mockUserIn;
-    private PrintWriter mockUserOut;
     private BufferedReader mockSocketIn;
     private PrintWriter mockSocketOut;
-
-    public ServerMockContainer(Socket mockSocket,
-                               BufferedReader mockUserIn,
-                               PrintWriter mockUserOut,
-                               BufferedReader mockSocketIn,
-                               PrintWriter mockSocketOut) {
-        this.mockSocket = mockSocket;
-        this.mockUserIn = mockUserIn;
-        this.mockUserOut = mockUserOut;
-        this.mockSocketIn = mockSocketIn;
-        this.mockSocketOut = mockSocketOut;
-    }
+    private PrintWriter mockLogOut;
 
     public ServerMockContainer(Socket mockSocket,
                                BufferedReader mockSocketIn,
@@ -30,24 +17,11 @@ public class ServerMockContainer {
         this.mockSocket = mockSocket;
         this.mockSocketIn = mockSocketIn;
         this.mockSocketOut = mockSocketOut;
-        this.mockUserIn = null;
-        this.mockUserOut = mockLogOut;
+        this.mockLogOut = mockLogOut;
     }
 
     public Socket getMockSocket() {
         return mockSocket;
-    }
-
-    public BufferedReader getMockUserIn() {
-        return mockUserIn;
-    }
-
-    public PrintWriter getMockUserOut() {
-        return mockUserOut;
-    }
-
-    public PrintWriter getMockLogOut() {
-        return mockUserOut;
     }
 
     public BufferedReader getMockSocketIn() {
@@ -56,5 +30,9 @@ public class ServerMockContainer {
 
     public PrintWriter getMockSocketOut() {
         return mockSocketOut;
+    }
+
+    public PrintWriter getMockLogOut() {
+        return mockLogOut;
     }
 }
